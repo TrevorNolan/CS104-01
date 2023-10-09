ed95e124580dbd52aa2274cb01391902e029f0ed
TrevorNolan <146225324+TrevorNolan@users.noreply.github.com>
2023-10-08 21:51:53 -0400

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,22 @@
+# CS104
+
+# Trevor Nolan
+
+# conditions.py
+
+temp="xyz"
+while temp != "end":
+    temp = int(input("Please enter the current tempurature: "))
+    if temp > (90):
+        print("Wear shorts")
+    elif (91) > temp > (70):
+        print("Short sleeves are fine")
+    elif (71) > temp > (50):
+        print("Wear a jacket")
+    elif (51) > temp > (32):
+        print("Wear a heavy coat")
+    else:
+        print("Stay Inside")
+        
+    
+
